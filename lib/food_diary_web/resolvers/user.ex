e501f8b77f8cf74ba9e840a45c16034e4b03ade0
neanderdev <neander.faria@hotmail.com>
2022-09-13 08:29:40 -0300

    Tipo Mutation

--- lib/food_diary_web/resolvers/user.ex
+++ lib/food_diary_web/resolvers/user.ex
@@ -2,4 +2,6 @@ defmodule FoodDiaryWeb.Resolvers.User do
   alias FoodDiary.Users
 
   def get(%{id: user_id}, _context), do: Users.Get.call(user_id)
+
+  def create(%{input: params}, _context), do: Users.Create.call(params)
 end
