5e616e7f35d1f9f494152ee6cf9ad481d28c0f1a
neanderdev <neander.faria@hotmail.com>
2022-09-13 08:34:22 -0300

    Deletando com Mutations

--- lib/food_diary_web/resolvers/user.ex
+++ lib/food_diary_web/resolvers/user.ex
@@ -4,4 +4,6 @@ defmodule FoodDiaryWeb.Resolvers.User do
   def get(%{id: user_id}, _context), do: Users.Get.call(user_id)
 
   def create(%{input: params}, _context), do: Users.Create.call(params)
+
+  def delete(%{id: id}, _context), do: Users.Delete.call(id)
 end
--- lib/food_diary_web/schema/types/root.ex
+++ lib/food_diary_web/schema/types/root.ex
@@ -3,22 +3,29 @@ defmodule FoodDiaryWeb.Schema.Types.Root do
 
   alias FoodDiaryWeb.Resolvers.User, as: UsersResolver
 
-  import_types FoodDiaryWeb.Schema.Types.User
+  import_types(FoodDiaryWeb.Schema.Types.User)
 
   object :root_query do
     field :user, type: :user do
-      arg :id, non_null(:id)
+      arg(:id, non_null(:id))
 
-      resolve &UsersResolver.get/2
+      resolve(&UsersResolver.get/2)
     end
   end
 
   object :root_mutation do
     @desc "Create a new user"
     field :create_user, type: :user do
-      arg :input, non_null(:create_user_input)
+      arg(:input, non_null(:create_user_input))
 
-      resolve &UsersResolver.create/2
+      resolve(&UsersResolver.create/2)
+    end
+
+    @desc "Deletes user"
+    field :delete_user, type: :user do
+      arg(:id, non_null(:id))
+
+      resolve(&UsersResolver.delete/2)
     end
   end
 end
